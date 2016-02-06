6ddfa77e856e6d44a837750a0b3e5edea53c309b
yk <ex.14.celsior@gmail.com>
2016-02-06 09:21:30 +0900

    Initial Commit

---
+++ countApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countApp
+//
+//  Created by yuki ishiguro on 2016/02/06.
+//  Copyright © 2016年 yk1209. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
